db3ddf10695278444c8917350755da7cc32e7a89
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-08-04 13:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
-//your code here
-let grandtotal = 0;
-const addData = () => {
-  let name = document.getElementById("item-name-input").value;
-  let qty = document.getElementById("item-qty-input").value;
-  let price = document.getElementById("item-price-input").value;
-  let table = document.getElementById("table1");
-  let newRow = table.insertRow(table.rows.length - 1);
-  let namecell = newRow.insertCell(0);
-  let qtycell = newRow.insertCell(1);
-  let pricecell = newRow.insertCell(2);
-  let totalprice = newRow.insertCell(3);
-  namecell.classList.add("item");
-  totalprice.classList.add("price");
-  namecell.innerHTML = name;
-  qtycell.innerHTML = qty;
-  pricecell.innerHTML = price;
-  totalprice.innerHTML = qty * price;
-  grandtotal += qty * price;
-  document.getElementById("total").innerHTML =
-    "Total Ammount is " + grandtotal + " $";
-  document.getElementById("myinputs").reset();
-};
+const products = [
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
+];
 
-// module.exports = addData;
+// DOM elements
+const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
 
-// button.addEventListener('click', addData);
+// Get cart data from session storage or initialize it
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+
+// Render product list
+function renderProducts() {
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    productList.appendChild(li);
+  });
+}
+
+// Render cart list
+function renderCart() {
+  cartList.innerHTML = "";
+  cart.forEach((cartItem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${cartItem.name} - $${cartItem.price} <button class="remove-from-cart-btn" data-id="${cartItem.id}">Remove from Cart</button>`;
+    cartList.appendChild(li);
+  });
+}
+
+// Add item to cart
+function addToCart(productId) {
+  const productToAdd = products.find((product) => product.id === productId);
+  if (productToAdd) {
+    cart.push(productToAdd);
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    renderCart();
+  }
+}
+
+// Remove item from cart
+function removeFromCart(productId) {
+  cart = cart.filter((cartItem) => cartItem.id !== productId);
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+}
+
+// Clear cart
+function clearCart() {
+  cart = [];
+  sessionStorage.removeItem("cart");
+  renderCart();
+}
+
+// Event listeners
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.getAttribute("data-id"));
+    addToCart(productId);
+  }
+});
+
+cartList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = parseInt(event.target.getAttribute("data-id"));
+    removeFromCart(productId);
+  }
+});
+
+clearCartBtn.addEventListener("click", clearCart);
+
+// Initial render
+renderProducts();
+renderCart();
